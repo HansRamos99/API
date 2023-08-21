98dd32e2101c64ef92b2fe859a5b7a8489c9aa08
unknown <hempghans@gmail.com>
2023-08-21 11:37:44 -0500

    a

--- psicologos/views.py
+++ psicologos/views.py
@@ -162,7 +162,7 @@ class CitasView(View):
             return JsonResponse(datos)
         
         else:
-
+            print("variable de entorno ---> "+USAR_REDIS)
             if(redis_instance.exists('citas') and USAR_REDIS):
                 print("usando redis")
                 datos = redis_instance.get('citas')
